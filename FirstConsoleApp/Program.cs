083511851e7984c590308a385bdc4d03b2225e31
Baraah <alsadibaraah@gmail.com>
2026-05-17 16:00:19 +0400

    added variable declarations

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace FirstConsoleApp
+﻿using System;
+
+namespace ClinicManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // patient variable
+            string patientName = "";
+            int patientAge = 0;
+            string patientPhone = "";
+            int patientCount = 0;
+            int MAX_PATIENTS = 2;
+
+            // doctor variable
+            string doctorName = "";
+            string doctorSpec = "";
+            int doctorCount = 0;
+            int MAX_DOCTORS = 2;
+
+            // appointment variable
+            string appointmentDate = "";
+            string appointmentTime = "";
+            bool appointmentBooked = false;
         }
     }
 }
