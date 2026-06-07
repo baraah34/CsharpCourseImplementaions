db97fb0d9b6afc98e292f29c889994e712301eb4
Baraah <alsadibaraah@gmail.com>
2026-06-07 11:35:34 +0400

    added case1 and 2

--- TS-DS-CAP-01/Program.cs
+++ TS-DS-CAP-01/Program.cs
@@ -64,7 +64,95 @@
         // Queue for waitlist passengers
         static Queue<string> waitlistQueue = new Queue<string>();
 
-      
+        // List to store cancelled tickets
+        static List<string> cancelledTickets = new List<string>();
+
+
+        //==================================================================================
+        //case 0
+        static void RegisterNewPassenger()
+        {
+            Console.WriteLine(" Register New Passenger ");
+
+            Console.Write("Enter passenger full name: ");
+            string name = Console.ReadLine(); // Read passenger name
+
+            if (name != null)
+            {
+                name = name.Trim(); // Remove spaces from start and end
+            }
+
+            // Check if name is empty
+            if (name == null || name.Length == 0)
+            {
+                Console.WriteLine("Error: Passenger name cannot be empty.");
+                return; // Stop function
+            }
+
+            // Check duplicate name
+            for (int i = 0; i < passengerNames.Count; i++)
+            {
+                if (passengerNames[i].ToLower() == name.ToLower())
+                {
+                    Console.WriteLine("Error: Passenger name already exists.");
+                    return;
+                }
+            }
+
+            // Generate next ticket number
+            int nextNumber = passengerNames.Count + 1;
+
+            // D3 means 3 digits with zeros
+            string newTicket = "TKT-" + nextNumber.ToString("D3");
+
+            // Add passenger name to passenger list
+            passengerNames.Add(name);
+
+            // Add ticket to ticket list
+            ticketNumbers.Add(newTicket);
+
+            Console.WriteLine("Passenger registered successfully.");
+            Console.WriteLine("Passenger Name: " + name);
+            Console.WriteLine("Ticket ID: " + newTicket);
+        }
+        //=============================================================================================
+        // CASE 02
+
+        static void ViewAllPassengers()
+        {
+            Console.WriteLine("===== View All Passengers =====");
+
+            if (passengerNames.Count == 0)
+            {
+                Console.WriteLine("No passengers registered yet.");
+                return;
+            }
+
+            Console.WriteLine("No. | Passenger Name           | Ticket ID | Status");
+            Console.WriteLine("----------------------------------------------------");
+
+            for (int i = 0; i < passengerNames.Count; i++)//start from zero then repeat it 
+            {
+                string status = "Active";
+
+                if (cancelledTickets.Contains(ticketNumbers[i])) //If TKT-001 exists inside cancelledTickets, that means the ticket is cancelled.
+                {
+                    status = "CANCELLED";
+                }
+
+                Console.WriteLine(
+                    (i + 1).ToString() +
+                    passengerNames[i] +
+                    ticketNumbers[i]+
+                    status
+                );
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Total passengers: " + passengerNames.Count);
+        }
+
+
         static void Main(string[] args)
         {
             bool mainMenu = true; // Controls the main menu loop
@@ -102,6 +190,7 @@
                         break;
 
                     case 2:
+                        ViewAllPassengers();
                         break;
 
                     case 3:
