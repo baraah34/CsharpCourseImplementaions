a260247268f48d6b174cf849bb49adfebb18f876
Baraah <alsadibaraah@gmail.com>
2026-06-08 08:10:42 +0400

    ADDED CASE 3 AND 4

--- TS-DS-CAP-01/Program.cs
+++ TS-DS-CAP-01/Program.cs
@@ -150,16 +150,162 @@
 
             Console.WriteLine("-------------------------------------------------");
             Console.WriteLine("Total passengers: " + passengerNames.Count);
-        }
 
+        }//============================================================================================
+        //case 3
+        static void BookFlightTicket()
+        {
+            Console.WriteLine("===== Book a Flight Ticket =====");
+
+            Console.Write("Enter ticket ID: ");
+            string ticket = Console.ReadLine();
+
+            if (ticket != null)
+            {
+                ticket = ticket.Trim().ToUpper(); 
+            }
+
+            // Find ticket index
+            int index = ticketNumbers.IndexOf(ticket);
+
+            // If index is -1, ticket does not exist
+            if (index == -1)
+            {
+                Console.WriteLine("Error: Ticket ID not found.");
+                return;
+            }
+
+            // Cancelled ticket cannot be booked
+            if (cancelledTickets.Contains(ticket))
+            {
+                Console.WriteLine("Error: Cancelled tickets cannot be booked.");
+                return;
+            }
+
+            // If ticket already exists in bookingRecord, it already has booking
+            if (bookingRecord.ContainsKey(ticket))
+            {
+                Console.WriteLine("This ticket already has a booking. Use update booking instead.");
+                return;
+            }
+
+            Console.WriteLine("\nAvailable Flights:");
+
+            // Display flight array
+            for (int i = 0; i < flightNumbers.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + flightNumbers[i]);
+            }
+            Console.WriteLine("Select flight number: ");
+            int flightChoice = int.Parse(Console.ReadLine());
+
+         
+
+            // Validate flight choice
+            if (flightChoice < 1 || flightChoice > flightNumbers.Length)
+            {
+                Console.WriteLine("Invalid flight selection.");
+                return;
+            }
+
+            // Get selected flight using index
+            string selectedFlight = flightNumbers[flightChoice - 1];
+
+            Console.WriteLine("\nAvailable Dates:");
+
+            // Display available dates
+            for (int i = 0; i < availableDates.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + availableDates[i]);
+            }
+
+            Console.WriteLine("Select  date: ");
+            int dateChoice = int.Parse(Console.ReadLine());
+
+            // Validate date choice
+            if (dateChoice < 1 || dateChoice > availableDates.Count)
+            {
+                Console.WriteLine("Invalid date selection.");
+                return;
+            }
+
+            // Get selected date
+            string selectedDate = availableDates[dateChoice - 1];
+
+            // Store booking in dictionary
+            // Key is ticket
+            // Value is flight|date
+            bookingRecord.Add(ticket, selectedFlight + "|" + selectedDate);
+
+            Console.WriteLine("\nBooking confirmed successfully.");
+            Console.WriteLine("Passenger Name: " + passengerNames[index]);
+            Console.WriteLine("Ticket ID: " + ticket);
+            Console.WriteLine("Flight: " + selectedFlight);
+            Console.WriteLine("Date: " + selectedDate);
+        }
+        //=============================================================================
+        //case 4
+        static void ViewBookingDetails()
+        {
+            Console.WriteLine("===== View Booking Details =====");
+            // ====from book flight function====
+            Console.Write("Enter ticket ID: ");
+            string ticket = Console.ReadLine();
+
+            if (ticket != null)
+            {
+                ticket = ticket.Trim().ToUpper();
+            }
+
+            int index = ticketNumbers.IndexOf(ticket);
+
+            // Validate ticket exists
+            if (index == -1)
+            {
+                Console.WriteLine("Error: Ticket ID not found.");
+                return;
+            }
+
+            // Check if ticket is cancelled
+            if (cancelledTickets.Contains(ticket))
+            {
+                Console.WriteLine("This ticket has been cancelled.");
+                return;
+            }
+
+            // Check if booking exists
+            if (!bookingRecord.ContainsKey(ticket))
+            {
+                Console.WriteLine("No booking found for this ticket.");
+                return;
+            }
+
+            // Get booking value from dictionary
+            string bookingValue = bookingRecord[ticket];
+
+            // Split flight and date using |
+            string[] parts = bookingValue.Split('|');
+
+            string flight = parts[0];
+            string date = parts[1];
+
+            Console.WriteLine("=================================");
+            Console.WriteLine("BOOKING SUMMARY");
+            Console.WriteLine("=================================");
+            Console.WriteLine("Passenger Name: " + passengerNames[index]);
+            Console.WriteLine("Ticket ID: " + ticket);
+            Console.WriteLine("Flight Number: " + flight);
+            Console.WriteLine("Date: " + date);
+            Console.WriteLine("=================================");
+        }
 
         static void Main(string[] args)
         {
-            bool mainMenu = true; // Controls the main menu loop
+            bool mainMenu = true; //  main menu loop
 
             while (mainMenu)
             {
-                Console.Clear(); // Clears screen before showing menu
+                Console.Clear(); 
 
                 Console.WriteLine("========================================");
                 Console.WriteLine("SKY WINGS FLIGHT MANAGEMENT SYSTEM");
@@ -194,9 +340,11 @@
                         break;
 
                     case 3:
+                        BookFlightTicket();
                         break;
 
                     case 4:
+                        ViewBookingDetails();
                         break;
 
                     case 5:
@@ -218,7 +366,7 @@
                         break;
 
                     case 0:
-                        mainMenu = false; // Stop the loop
+                        mainMenu = false; // Stop
                         Console.WriteLine("Thank you for using Sky Wings Flight Management System.");
                         break;
 
@@ -227,7 +375,6 @@
                         break;
                 }
 
-                // If user did not exit, pause before returning to menu
                 if (mainMenu)
                 {
                     Console.WriteLine("\nPress any key to continue...");
